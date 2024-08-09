a7dc448f8b8913dea6f7b36b2ce2eea6fa537eb8
asif <rimbaborne@gmail.com>
2024-08-09 23:34:30 +0800

    init

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,19 @@
-import './bootstrap';
+import "@protonemedia/laravel-splade/dist/style.css";
+import 'flowbite';
+import "../css/app.css";
+import "./bootstrap";
+
+import { renderSpladeApp, SpladePlugin } from "@protonemedia/laravel-splade";
+import { createApp } from "vue/dist/vue.esm-bundler.js";
+
+const el = document.getElementById("app");
+
+createApp({
+    render: renderSpladeApp({ el })
+})
+    .use(SpladePlugin, {
+        "max_keep_alive": 10,
+        "transform_anchors": false,
+        "progress_bar": true
+    })
+    .mount(el);
